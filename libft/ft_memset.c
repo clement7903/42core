9cac3d5ae21bb15da8ec7ba3488342f98f3746c4
clement7903 <clement7903@hotmail.com>
2023-09-17 22:32:50 +0800

    update till ft_memset

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,21 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <cng>                               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/17 22:16:08 by marvin            #+#    #+#             */
-/*   Updated: 2023/09/17 22:16:08 by marvin           ###   ########.fr       */
+/*   Created: 2023/09/17 22:16:23 by marvin            #+#    #+#             */
+/*   Updated: 2023/09/17 22:16:23 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-int	ft_isdigit(char c)
+void	*ft_memset(void *pointer, int c, size_t num)
 {
-	if (c >= '0' && c <= '9')
+	char	*p;
+
+	if (!pointer)
 	{
-		return (1);
+		return (NULL);
 	}
-	return (0);
+	p = (char *)pointer;
+	while (num > 0)
+	{
+		*p = c;
+		p++;
+		num--;
+	}
+	return (pointer);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -1,11 +1,16 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <unistd.h>
+# include <stdlib.h>
+
 // Header file contents go here
-int	ft_isalpha(int c);
-int	ft_isdigit(char c);
-int	ft_isalnum(char c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int			ft_isalpha(int c);
+int			ft_isdigit(char c);
+int			ft_isalnum(char c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+size_t		ft_strlen(const char *str);
+void		*ft_memset(void *pointer, int c, size_t num);
 
 #endif
