5d684c94a1e382c6a88d43dae74c1045bb2a7c89
clement7903 <clement7903@hotmail.com>
2023-09-19 14:55:22 +0800

    ft_memcpy.c

--- libft/libft.h
+++ libft/libft.h
@@ -17,5 +17,6 @@ char		ft_tolower(char c);
 int			ft_strncmp(const char *str1, const char *str2, size_t n);
 void		ft_bzero(void *ptr, size_t size);
 void		*ft_memchr(const void *ptr, int value, size_t num);
+void		ft_memcpy(void *dst, const void *src, size_t num);
 
 #endif
