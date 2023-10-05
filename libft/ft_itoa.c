9e82820218c508e14e513505db520422ac2f2555
clement7903 <clement7903@hotmail.com>
2023-10-06 01:34:39 +0800

    add ft_itoa.c

---
+++ libft/ft_itoa.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <cng>                               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 01:27:03 by marvin            #+#    #+#             */
+/*   Updated: 2023/10/06 01:27:03 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+/*
+PURPOSE: 	converts an integet to a string representation
+
+nbr_len function:
+- count number of digits in nbr
+
+abs_val function:
+- provides absolute value of the number
+
+str_new function:
+- allocates mem for the new string
+*/
+static int	nbr_len(int nbr)
+{
+	int	len;
+
+	len = 0;
+	if (nbr < 1)
+		len++;
+	while (nbr)
+	{
+		nbr /= 10;
+		len++;
+	}
+	return (len);
+}
+
+static long long	abs_val(long long n)
+{
+	long long	nb;
+
+	nb = 1;
+	if (n < 0)
+		nb *= -n;
+	else
+		nb *= n;
+	return (nb);
+}
+
+static char	*str_new(size_t n)
+{
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * (n + 1));
+	if (!str)
+		return (NULL);
+	return (str);
+}
+
+char	*ft_itoa(int n)
+{
+	unsigned int	nbr;
+	int				sign;
+	int				len;
+	char			*str;
+
+	sign = 0;
+	if (n < 0)
+		sign = 1;
+	len = nbr_len(n);
+	str = str_new(len);
+	if (!str)
+		return (NULL);
+	*(str + len) = '\0';
+	nbr = abs_val(n);
+	while (len--)
+	{
+		*(str + len) = 48 + nbr % 10;
+		nbr /= 10;
+	}
+	if (sign)
+		*(str) = 45;
+	return (str);
+}
